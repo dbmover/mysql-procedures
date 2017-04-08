88602d6e5fcf9685129afe8db30b06e941264d2a
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-08 12:16:36 +0200

    mysql procedure plugin

---
+++ src/Plugin.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @package Dbmover
+ * @subpackage Mysql
+ * @subpackage Procedures
+ */
+
+namespace Dbmover\Mysql\Procedures;
+
+use Dbmover\Procedures;
+
+class Plugin extends Procedures\Plugin
+{
+    const DROP_ROUTINE_SUFFIX = '';
+}
+
